59de6443b17800967c3c8377bb67e7a98b31ceba
Matt <fortiek@gmail.com>
2022-12-18 22:28:47 -0500

    Up to pt 4 now, slow going and hard to get the brain around. Looks C based

--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,23 @@ package main
 import (
 	"fmt"
 	"example.com/greetings"
+	"log"
 )
 
 func main() {
 	//Call locally created imported function
-	message := greetings.Hello("Archer")
+//--PT 2--
+//Imported the "log" default Go packages for logging, implement them now
+log.SetPrefix("greetings: ")
+log.SetFlags(0)
+
+//Request a greeting
+	message, err := greetings.Hello("Archer")
+	//call error handling before attempting to finish successfully
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Print function results to term if no errors are returned
 	fmt.Println(message)
 }
